e537bcb63d6fe83ed0afde56634c5a6c12f2a644
smuktevi <vmuktev1@uw.edu>
2021-01-29 23:42:04 -0600

    all tests failed autograder after removing @ test so added it back.

--- tests/test_email_validity.py
+++ tests/test_email_validity.py
@@ -7,10 +7,10 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import newsgroup_analysis as nga
 class TestNewsGroupAnalysis(unittest.TestCase):
 
-    # def test_checkemail_at(self):
-    #     """confirm that check_email_validity() checks for @ sign"""
-    #     self.assertFalse(nga.check_email_validity("emailnoat.com"),"missing the @ symbol")
-    #     self.assertFalse(nga.check_email_validity("email@noat.c@om"), "2 @ symbols")
+    def test_checkemail_at(self):
+        """confirm that check_email_validity() checks for @ sign"""
+        self.assertFalse(nga.check_email_validity("emailnoat.com"),"missing the @ symbol")
+        self.assertFalse(nga.check_email_validity("email@noat.c@om"), "2 @ symbols")
     
     def test_emailformaterror(self):
         """confirm that a non-email format raises EmailFormatError"""
